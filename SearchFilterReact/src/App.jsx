f5821500b8e1eebde9a674022cf5958f742e585e
SanuthiVihansa <subasofts@gmail.com>
2023-05-27 09:07:23 +0530

    commit modified changes

--- SearchFilterReact/src/App.jsx
+++ SearchFilterReact/src/App.jsx
@@ -1,36 +1,34 @@
-import './App.css'
-import React,{useState} from "react";
-import'./styles.css'
+import React, { useState } from "react";
+import './App.css';
+import './styles.css';
 
 function App() {
-//creating an array
+  const list = ["Banana", "Apple", "Orange", "Mango", "Pineapple", "Watermelon"];
+  const [filterList, setFilterList] = useState(list);
 
-const list = ["Banana","Apple","Orange","Mango","Pineapple","Watermelon"];
-const [filterList,setFilterList]= useState(list);
-
-const handleSearch =(event)=>{
-  if(event.target.value===""){
-    setFilterList(list);
-    return;
-  }
-  const filteredValues = list.filter(
-    (item)=>item.toLowerCase().indexOf(event.target.value.toLowerCase())!==-1);
+  const handleSearch = (event) => {
+    if (event.target.value === "") {
+      setFilterList(list);
+      return;
+    }
+    const filteredValues = list.filter(
+      (item) => item.toLowerCase().indexOf(event.target.value.toLowerCase()) !== -1
+    );
     setFilterList(filteredValues);
-};
+    
+  };
+
 
   return (
     <div className='app'>
       <div>
-        search:<input name='query' type="text" onChange={handleSearch}/>
+        search: <input name='query' type="text" onChange={handleSearch} />
       </div>
-      {filterList && filterList.map((item,index)=>{
-        return(
+      {filterList && filterList.map((item, index) => (
         <div key={index}>{item}</div>
-        );
-      })}
-      
+      ))}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
